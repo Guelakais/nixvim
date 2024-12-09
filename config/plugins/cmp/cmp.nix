bc02350188a8b9d96b403f767054d032f78440dc
GueLaKais <koroyeldiores@gmail.com>
2024-12-09 18:32:21 +0100

    autocompletion does now work properly

--- config/plugins/cmp/cmp.nix
+++ config/plugins/cmp/cmp.nix
@@ -5,19 +5,26 @@
     };
     cmp = {
       enable = true;
+      filetype = {
+        sql.sources = [
+          { name = "buffer"; }
+          { name = "vim-dadbod-completion"; }
+        ];
+      };
       settings = {
         autoEnableSources = true;
-        experimental = {
-          ghost_text = true;
-        };
+        completion.completeopt = "menu,menuone,noinsert";
+        experimental.ghost_text = true;
         performance = {
           debounce = 60;
           fetchingTimeout = 200;
           maxViewEntries = 30;
         };
-        snippet = {
-          expand = "luasnip";
-        };
+        snippet.expand = ''
+          function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        '';
         formatting = {
           fields = [
             "kind"
@@ -26,44 +33,76 @@
           ];
         };
         sources = [
-          { name = "git"; }
-          { name = "nvim_lsp"; }
-          { name = "emoji"; }
           {
             name = "buffer"; # text within current buffer
             option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
             keywordLength = 3;
+            priority = 50;
           }
-          { name = "copilot"; }
+          {
+            name = "git";
+            priority = 20;
+          }
+          {
+            name = "nvim_lsp";
+            priority = 100;
+          }
+          {
+            name = "emoji";
+            priority = 5;
+          }
+          {
+            name = "copilot";
+            priority = 60;
+          }
           {
             name = "path"; # file system paths
             keywordLength = 3;
+            priority = 30;
           }
           {
             name = "luasnip"; # snippets
             keywordLength = 3;
+            priority = 70;
           }
         ];
 
         window = {
-          completion = {
-            border = "solid";
-          };
-          documentation = {
-            border = "solid";
-          };
+          completion.border = "rounded";
+          documentation.border = "rounded";
         };
 
         mapping = {
-          "<C-Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<Tab>".__raw = ''
+            cmp.mapping(function(fallback)
+              local luasnip = require("luasnip")
+              if luasnip.locally_jumpable(1) then
+                luasnip.jump(1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
+          "<S-Tab>".__raw = ''
+            cmp.mapping(function(fallback)
+              local luasnip = require("luasnip")
+              if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
           "<C-e>" = "cmp.mapping.abort()";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
           "<C-CR>" = "cmp.mapping.confirm({ select = true })";
           "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+          "<Up>" = # lua
+            "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Down>" = # lua
+            "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
         };
       };
     };
@@ -81,6 +120,7 @@
     }; # snippets
     cmp-cmdline = {
       enable = true;
+
     }; # autocomplete for cmdline
   };
   extraConfigLua = ''
