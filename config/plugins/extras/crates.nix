bef03d51e34ac2469ed9db65794b6f92fa298c1c
GueLaKais <koroyeldiores@gmail.com>
2024-12-09 14:17:51 +0100

    added correct crates-nvim declaration

--- config/plugins/extras/crates.nix
+++ config/plugins/extras/crates.nix
@@ -1,3 +1,18 @@
 {
-  plugins.crates-nvim.enable = true;
+  plugins.crates-nvim = {
+    enable = true;
+    extraOptions = {
+      completion = {
+        crates = {
+          enabled = true;
+        };
+      };
+      lsp = {
+        enabled = true;
+        actions = true;
+        completion = true;
+        hover = true;
+      };
+    };
+  };
 }
