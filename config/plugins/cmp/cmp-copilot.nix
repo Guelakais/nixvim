251a4794f709fd3141c172328c7110f0c7a3dfbf
GueLaKais <koroyeldiores@gmail.com>
2024-12-09 09:15:53 +0100

    enabled copilot suggestions

--- config/plugins/cmp/cmp-copilot.nix
+++ config/plugins/cmp/cmp-copilot.nix
@@ -14,7 +14,7 @@
 
   extraConfigLua = ''
     require("copilot").setup({
-      suggestion = { enabled = false },
+      suggestion = { enabled = true },
       panel = { enabled = false },
     })
   '';
